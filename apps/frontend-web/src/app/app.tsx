4eda87c96de7a62ba146baf1ca53c0ad08ec93ae
Joe Hill <hillj1895@yahoo.com>
2024-09-26 12:56:26 -0600

    refactor(shared): added missed FE consume of shared types

--- apps/frontend-web/src/app/app.tsx
+++ apps/frontend-web/src/app/app.tsx
@@ -1,8 +1,10 @@
+import { Message } from '@master-skeleton-full-stack/types'
 
 export function App() {
+  const message: Message = { message: 'Hello world' };
   return (
     <div>
-      <div className="text-center">HELLO WORLD</div>
+      <div className="text-center">{message.message}</div>
     </div>
   );
 }
